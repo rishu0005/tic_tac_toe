58565e75d5795ec03ecd4d7704abaa004c3438aa
rishu0005 <rishusingh5943@gmail.com>
2025-06-27 12:51:13 +0530

    made a dummy tic tac toe version and it do nothing but let you click only firsrsquaree

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,13 +1,9 @@
 import {useState} from 'react' ;
 
-function Square({value}){
-  // const [value, setValue] = useState(null);
-
-  // function handleClick(){
-  //   setValue('X');
-  // }
+function Square({value, onSquareClick}){
+  
   return(  
-        <button className="square"> 
+        <button className="square" onClick={onSquareClick}> 
             {value}
            </button>
       );
@@ -17,10 +13,17 @@ function Square({value}){
 export default function Board(){
     const [squares, setSquares] = useState(Array(9).fill(null));
 
+    function  handleClick(i){
+      const nextSquares  = squares.slice();
+      nextSquares[i] = "X";
+      setSquares(nextSquares);
+
+      }
+
   return(
     <>
     <div className="board-row">
-      <Square value={squares[0]} />
+      <Square value={squares[0]} onSquareClick={handleClick} />
       <Square value={squares[1]} />
       <Square value={squares[2]} />
     </div>
